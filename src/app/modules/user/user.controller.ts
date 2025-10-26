99aeb0548f5ac04e39c2c363cac8b77d9ef93289
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-26 14:21:24 +0600

    57-5 Handling Image Upload using Multer

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,20 @@
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import { UserService } from "./user.service";
+import sendResponse from "../../shared/sendResponse";
 
 const createPatient = catchAsync(async (req: Request, res: Response) => {
-    const result = await UserService.createPatient(req.body);
+    // const result = await UserService.createPatient(req.body);
+
+    console.log(req.body)
     
     
-    res.status(200).json({
+    sendResponse(res, {
+        statusCode: 201,
         success: true,
-        message: 'Patient created successfully',
-        data: req.body
-    });
+        message: "Patient created successfully!",
+        data: ""
+    })
 });
 
 export const UserController = {
