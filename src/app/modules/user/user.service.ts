1264ca45aaf79d6a1cce4189c118c6abf69050a8
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-26 14:33:40 +0600

    57-6 Parsing Data & Preparing Image for Cloudinary Upload

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,27 +1,32 @@
 import bcrypt from "bcryptjs";
-import { createPatientInput } from "./user.interface"
+import { Request } from "express";
 import { prisma } from "../../shared/prisma";
+import { fileUploader } from "../../helpers/fileUploader";
 
-const createPatient = async (payload: createPatientInput) => {
-    const hashPassword = await bcrypt.hash(payload.password, 10);
+const createPatient = async (req: Request) => {
+    if (req.file) {
+        const uploadResult = await fileUploader.uploadToCloudinary(req.file)
+        console.log("uploadResult : ", uploadResult)
+    }
+    // const hashPassword = await bcrypt.hash(req.body.password, 10);
 
-    const result = await prisma.$transaction(async (tnx) => {
-        await tnx.user.create({
-            data: {
-                email: payload.email,
-                password: hashPassword
-            }
-        });
+    // const result = await prisma.$transaction(async (tnx) => {
+    //     await tnx.user.create({
+    //         data: {
+    //             email: req.body.email,
+    //             password: hashPassword
+    //         }
+    //     });
 
-        return await tnx.patient.create({
-            data: {
-                name: payload.name,
-                email: payload.email
-            }
-        })
-    })
+    //     return await tnx.patient.create({
+    //         data: {
+    //             name: req.body.name,
+    //             email: req.body.email
+    //         }
+    //     })
+    // })
 
-    return result;
+    // return result;
 
 }
 
