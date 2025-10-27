3430363eb2db3575d5d6f6928b8a04059c5032c6
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-27 10:06:22 +0600

    59-6 Retrieving Schedules for a Specific Doctor

--- src/app/modules/schedule/schedule.route.ts
+++ src/app/modules/schedule/schedule.route.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import { ScheduleController } from "./schedule.controller";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
+
+router.get(
+    "/",
+    auth(UserRole.DOCTOR, UserRole.DOCTOR),
+    ScheduleController.schedulesForDoctor
+)
 
 router.post(
     "/",
--- src/app/modules/schedule/schedule.service.ts
+++ src/app/modules/schedule/schedule.service.ts
@@ -1,5 +1,7 @@
 import { addMinutes, addHours, format } from "date-fns";
 import { prisma } from "../../shared/prisma";
+import { IOptions, paginationHelper } from "../../helpers/paginationHelper";
+import { Prisma } from "@prisma/client";
 
 const insertIntoDB = async (payload: any) => {
 
@@ -63,7 +65,60 @@ const insertIntoDB = async (payload: any) => {
     return schedules;
 }
 
+const schedulesForDoctor = async (filters: any , options: IOptions) => {
+    const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+    const { startDateTime: filterStartDateTime, endDateTime: filterEndDateTime } = filters;    
+
+    const andConditions: Prisma.ScheduleWhereInput[] = [];
+
+    if (filterStartDateTime && filterEndDateTime) {
+        andConditions.push({
+            AND: [
+                {
+                    startDateTime: {
+                        gte: filterStartDateTime
+                    }
+                },
+                {
+                    endDateTime: {
+                        lte: filterEndDateTime
+                    }
+                }
+            ]
+        })
+    }
+
+    const whereConditions: Prisma.ScheduleWhereInput = andConditions.length > 0 ? {
+        AND: andConditions
+    } : {}
+
+    const result = await prisma.schedule.findMany({
+        where: whereConditions,
+        skip,
+        take: limit,
+        orderBy: {
+            [sortBy]: sortOrder
+        }
+    });
+
+    const total = await prisma.schedule.count({
+        where: whereConditions,
+    });
+
+    return {
+        meta: {
+            page,
+            limit,
+            total
+        },
+        data: result
+    };
+
+
+}
+
 
 export const ScheduleService = {
   insertIntoDB,
+  schedulesForDoctor
 };
