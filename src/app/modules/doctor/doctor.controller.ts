2a1b72125b75a7752f21744e5b6bc4a203ead0a3
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-28 10:34:12 +0600

    60-6 Implementing Doctor Searching Functionality

---
+++ src/app/modules/doctor/doctor.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import pick from "../../helpers/pick";
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { DoctorService } from "./doctor.service";
+import { doctorFilterableFields } from "./doctor.constant";
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
+    const fillters = pick(req.query, doctorFilterableFields)
+
+    const result = await DoctorService.getAllFromDB(fillters, options);
+
+    sendResponse(res, {
+        statusCode: 200,
+        success: true,
+        message: "Doctor fetched successfully!",
+        data: result
+    })
+})
+
+
+export const DoctorController = {
+    getAllFromDB
+}
---
+++ src/app/modules/doctor/doctor.service.ts
@@ -0,0 +1,39 @@
+import { Prisma } from "@prisma/client";
+import { IOptions, paginationHelper } from "../../helpers/paginationHelper";
+import { doctorSearchableFields } from "./doctor.constant";
+
+const getAllFromDB = async (filters: any, options: IOptions) => {
+    const { page, limit, skip, sortBy, sortOrder } = paginationHelper.calculatePagination(options);
+    const { searchTerm, specialties, ...filterData } = filters;
+
+    const andConditions: Prisma.DoctorWhereInput[] = [];
+
+    if (searchTerm) {
+        andConditions.push({
+            OR: doctorSearchableFields.map((field) => ({
+                [field]: {
+                    contains: searchTerm,
+                    mode: "insensitive"
+                }
+            }))
+        })
+    }
+
+    if (Object.keys(filterData).length > 0) {
+        const filterConditions = Object.keys(filterData).map((key) => ({
+            [key]: {
+                equals: (filterData as any)[key]
+            }
+        }))
+
+        andConditions.push(...filterConditions)
+    }
+
+
+}
+
+
+export const DoctorService = {
+    getAllFromDB
+}
+
