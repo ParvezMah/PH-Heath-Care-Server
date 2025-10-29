557b22196f499b6e2dd1c4229f098ddc86f6d93d
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-29 10:31:57 +0600

    62-2 Stripe Payment Implementation – Part 2

---
+++ src/app/modules/payment/payment.routes.ts
@@ -0,0 +1,22 @@
+/*
+
+// router.post(
+//   "/webhook",
+//   express.raw({ type: "application/json" }), // important for signature verification
+//   stripeWebhookHandler
+// );
+
+
+
+payments post route ta amra ekhane korle problem hobe
+
+❓❓❓CORS Issue from app.ts❓❓❓
+
+endpoint url ta set korte hobe stripe er webhook er modde
+
+ei jonno amra app.ts
+
+app.use(cors()) er upore endoint ta set korbo
+
+
+*/
