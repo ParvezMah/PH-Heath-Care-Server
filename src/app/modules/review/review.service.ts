9087b0b3d5482e99b727ae9d348b74d690fa1424
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-10-29 19:35:29 +0600

    63-2 Creating Review – Part 2

--- src/app/modules/review/review.service.ts
+++ src/app/modules/review/review.service.ts
@@ -20,7 +20,37 @@ const insertIntoDB = async (user: IJWTPayload, payload: any) => {
     if (patientData.id !== appointmentData.patientId) {
         throw new ApiError(httpStatus.BAD_REQUEST, "This is not your appointment!")
     }
-    
+    return await prisma.$transaction(async (tnx) => {
+        const result = await tnx.review.create({
+            data: {
+                appointmentId: appointmentData.id,
+                doctorId: appointmentData.doctorId,
+                patientId: appointmentData.patientId,
+                rating: payload.rating,
+                comment: payload.comment
+            }
+        });
+
+        const avgRating = await tnx.review.aggregate({
+            _avg: {
+                rating: true
+            },
+            where: {
+                doctorId: appointmentData.doctorId
+            }
+        })
+
+        await tnx.doctor.update({
+            where: {
+                id: appointmentData.doctorId
+            },
+            data: {
+                averageRating: avgRating._avg.rating as number
+            }
+        })
+
+        return result;
+    })
 };
 
 export const ReviewService = {
