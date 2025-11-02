bb6dc04d44fcd31ffa288918a6e126db6ceadb55
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-02 07:24:16 +0600

    63-6 Overview of Change Password, Refresh Token, Forgot & Reset Password Features

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,5 +14,19 @@ export default {
     },
     openRouterApiKey: process.env.OPENROUTER_API_KEY,
     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    stripeWebhookSecreyKey: process.env.STRIPE_WEBHOOK_SECKRET_KEY
+    stripeWebhookSecreyKey: process.env.STRIPE_WEBHOOK_SECKRET_KEY,
+    emailSender: {
+        email: process.env.EMAIL,
+        app_pass: process.env.APP_PASS
+    },
+    jwt: {
+        jwt_secret: process.env.JWT_ACCESS_SECRET,
+        expires_in: process.env.JWT_ACCESS_EXPIRES,
+        refresh_token_secret: process.env.JWT_REFRESH_SECRET,
+        refresh_token_expires_in: process.env.JWT_REFRESH_EXPIRES,
+        reset_pass_secret: process.env.RESET_PASS_TOKEN,
+        reset_pass_token_expires_in: process.env.RESET_PASS_TOKEN_EXPIRES_IN
+    },
+    salt_round: process.env.BCRYPT_SALT_ROUND,
+    reset_pass_link: process.env.RESET_PASS_LINK
 }
