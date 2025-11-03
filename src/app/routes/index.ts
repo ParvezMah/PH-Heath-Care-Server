5135256afc8c26637b19b44f2b77294de7270572
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2025-11-03 15:30:03 +0600

    64-4 Fetching Dashboard Metadata – Part 1

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -10,6 +10,7 @@ import { PrescriptionRoutes } from '../modules/prescription/prescription.routes'
 import { ReviewRoutes } from '../modules/review/review.routes';
 import { PatientRoutes } from '../modules/patient/patient.routes';
 import { AdminRoutes } from '../modules/admin/admin.routes';
+import { MetaRoutes } from '../modules/meta/meta.routes';
 
 const router = express.Router();
 
@@ -57,6 +58,10 @@ const moduleRoutes = [
     {
         path: '/review',
         route: ReviewRoutes
+    },
+        {
+        path: '/metadata',
+        route: MetaRoutes
     },
     // Node Cron Jobs can be added here
 ];
